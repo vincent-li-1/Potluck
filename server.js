3bf70a6bb9a364938a706502206b10b510cc69bc
Vincent Li <vincentfli.1017@gmail.com>
2023-06-11 17:38:59 -0500

    aws test

--- server.js
+++ server.js
@@ -13,30 +13,30 @@ MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
 	.then(client => {
 		console.log(`Connected to ${dbName} database`);
 		db = client.db(dbName);
-	})
-	.catch(error => console.error(error));
+		app.set('view engine', 'ejs');
+		app.use(express.static('public'));
+		app.use(express.urlencoded({extended: true}));
+		app.use(express.json());
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-app.get('/', (req, res) => {
-	db.collection('recipes').find().toArray()
-	.then(data => {
-		res.render('index.ejs', {recipeList: data});
-	})
-	.catch(error => console.error(error));
-})
-
-app.post('/addRecipe', (req, res) => {
-	const recipeToAdd = new Recipe(req.body.recipeName, req.body.ingredients, req.body.steps);
-	db.collection('recipes').insertOne(recipeToAdd)
-		.then(result => {
-			console.log('Recipe added');
-			res.redirect('/')
+		app.get('/', (req, res) => {
+			db.collection('recipes').find().toArray()
+			.then(data => {
+				res.render('index.ejs', {recipeList: data});
+			})
+			.catch(error => console.error(error));
 		})
-		.catch(error => console.error(error));
-})
 
-app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}`));
+		app.post('/addRecipe', (req, res) => {
+			const recipeToAdd = new Recipe(req.body.recipeName, req.body.ingredients, req.body.steps);
+			db.collection('recipes').insertOne(recipeToAdd)
+				.then(result => {
+					console.log('Recipe added');
+					res.redirect('/')
+				})
+				.catch(error => console.error(error));
+		})
+
+		app.listen(process.env.PORT || PORT, () => console.log(`Server running on port ${PORT}`));
+			})
+	.catch(error => console.error(error));
+
